5d10c6f3a6a0523c22f986078cc423444c336f36
Gerald Baalham <geraldbaalham@live.co.uk>
2025-11-27 14:28:00 +0000

    feat: add UI components including Skeleton, Slider, Sonner, Spinner, Switch, Table, Tabs, Textarea, Toggle Group, Toggle, Tooltip, and their respective styles
    feat: generate Convex API and data model types for improved type safety
    
    feat: implement mobile detection hook for responsive design
    
    feat: initialize Inngest client for event handling
    
    fix: utility function for class name merging with Tailwind CSS

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
+import { ConvexClientProvider } from "@/components/ConvexClientProvider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,12 +26,13 @@ export default function RootLayout({
 }>) {
   return (
     <ClerkProvider>
+      <ConvexClientProvider>
     <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
       </body>
-    </html></ClerkProvider>
+    </html></ConvexClientProvider></ClerkProvider>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,21 @@
+import { CtaSection } from "@/components/home/cta-section";
+import { FeaturesSection } from "@/components/home/features-section";
+import { Footer } from "@/components/home/footer";
+import { Header } from "@/components/home/Header";
+import { HeroSection } from "@/components/home/hero-section";
+import { PricingSection } from "@/components/home/pricing-section";
+import { Button } from "@/components/ui/button";
 
 
 export default function Home() {
   return (
     <div className="text-3xl">
-      hello again
+      <Header/>
+      <HeroSection/>
+      <FeaturesSection/>
+      <PricingSection/>
+      <CtaSection/>
+      <Footer/>
     </div>
   );
 }
